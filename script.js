3ba2aa23ed46aad897572cb88fa96676425ea9a1
Nicole Zheng <zznicole@hotmail.com>
2020-07-22 21:01:58 +0200

    fix search todo list

--- script.js
+++ script.js
@@ -65,8 +65,9 @@ function myFunction() {
   // Loop through all list items, and hide those who don't match the search query
   for (i = 0; i < li.length; i++) {
     console.log(li[i]);
-    a = li[i].getElementsByTagName("li")[0];
-    txtValue =  a.innerText;
+    // fixed search issue by deleting .getElementsByTagName("li")[0] which was after li[i] on line 69
+    a = li[i];
+    txtValue = a.textContent || a.innerText;
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
       li[i].style.display = "";
     } else {
