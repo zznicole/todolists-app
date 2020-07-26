8d4924a15a7ad86a8635d54fd348fa09f32d623a
Nora Kristensson <kristensson.nora@gmail.com>
2020-07-27 01:57:25 +0200

    censoring bad words

--- script.js
+++ script.js
@@ -1,78 +1,82 @@
 // Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
+var myNodelist = document.getElementsByTagName('LI');
 var i;
 for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
+	var span = document.createElement('SPAN');
+	var txt = document.createTextNode('\u00D7');
+	span.className = 'close';
+	span.appendChild(txt);
+	myNodelist[i].appendChild(span);
 }
 
 // Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
+var close = document.getElementsByClassName('close');
 var i;
 for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
+	close[i].onclick = function () {
+		var div = this.parentElement;
+		div.style.display = 'none';
+	};
 }
 
 // Add a "checked" symbol when clicking on a list item
 var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
+list.addEventListener(
+	'click',
+	function (ev) {
+		if (ev.target.tagName === 'LI') {
+			ev.target.classList.toggle('checked');
+		}
+	},
+	false
+);
 
 // Create a new list item when clicking on the "Add" button
 function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
+	var li = document.createElement('li');
+	var inputValue = document.getElementById('myInput').value;
+	var t = document.createTextNode(inputValue);
+	li.appendChild(t);
+	if (inputValue === '') {
+		alert('You must write something!');
+	} else {
+		document.getElementById('myUL').appendChild(li);
+	}
+	document.getElementById('myInput').value = '';
 
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
+	var span = document.createElement('SPAN');
+	var txt = document.createTextNode('\u00D7');
+	span.className = 'close';
+	span.appendChild(txt);
+	li.appendChild(span);
 
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
-  }
+	for (i = 0; i < close.length; i++) {
+		close[i].onclick = function () {
+			var div = this.parentElement;
+			div.style.display = 'none';
+		};
+	}
 }
 
 function myFunction() {
-  // Declare variables
-  var input, filter, ul, li, a, i, txtValue;
-  input = document.getElementById('searchInput');
-  filter = input.value.toUpperCase();
-  ul = document.getElementById("myUL");
-  li = document.getElementsByTagName('li');
-  // Loop through all list items, and hide those who don't match the search query
-  for (i = 0; i < li.length; i++) {
-    console.log(li[i]);
-    // fixed search issue by deleting .getElementsByTagName("li")[0] which was after li[i] on line 69
-    a = li[i];
-    txtValue = a.textContent || a.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
-    } else {
-      li[i].style.display = "none";
-    }
-  }
+	// Declare variables
+	var input, filter, ul, li, a, i, txtValue;
+	input = document.getElementById('searchInput');
+	filter = input.value.toUpperCase();
+	ul = document.getElementById('myUL');
+	li = document.getElementsByTagName('li');
+	// Loop through all list items, and hide those who don't match the search query
+	for (i = 0; i < li.length; i++) {
+		console.log(li[i]);
+		// fixed search issue by deleting .getElementsByTagName("li")[0] which was after li[i] on line 69
+		a = li[i];
+		txtValue = a.textContent || a.innerText;
+		if (txtValue.toUpperCase().indexOf(filter) > -1) {
+			li[i].style.display = '';
+		} else {
+			li[i].style.display = 'none';
+		}
+	}
 }
 
 myFunction();
