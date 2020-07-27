7f3e6775b10c3e80b6d5aeee3f6898c85fefdca2
michelleandersson <michelleandersson@live.com>
2020-07-27 16:12:08 +0200

    Cleaned up code

--- test-script.js
+++ test-script.js
@@ -7,6 +7,8 @@ const completed = document.querySelector('.completed');
 const selectAll = document.querySelector('.far');
 let switchIt = false;
 
+
+//create an id for each todo which is how we will track each todo for completion and deletion
 function randomGen(str = 'qwertyuio') {
   return str.split('')
     .sort(() => Math.random() - 0.5)
@@ -17,18 +19,18 @@ class Todos{
   constructor(){
     this.todo = [];
   }
-    addTodo(){
-      this.todo = this.todo.concat({
-      name: mainInput.value,
-      completed: false,
-      id: randomGen()
-      })
-    }
+  addTodo(){
+    this.todo = this.todo.concat({
+    name: mainInput.value,
+    completed: false,
+    id: randomGen()
+    })
+  }
 
 
-    render(parent){
-      ul.innerHTML = '';
-      this.todo.forEach(t =>{
+  render(parent){
+    ul.innerHTML = '';
+    this.todo.forEach(t =>{
       let li =document.createElement('li');
       li.classList.add('li');
       let p = document.createElement('p');
@@ -46,8 +48,6 @@ class Todos{
       deleteit.textContent = 'X'
       li.append(input,p,deleteit);
       parent.append(li);
-
-
 
       //replace para with input box on doubleclick
       const replaceItFunc = function(event){
@@ -68,21 +68,10 @@ class Todos{
           newtodo.render(ul);
         })
       }
-
-
       p.addEventListener('dblclick',replaceItFunc);
       //completed replace para with inputbox on doubleclick
-
-
-
-      //add a message if the todo list is empty
-      // function emptyList(){
-      //   if(newtodo.todo.length !== 0) {
-      //     document.querySelector(".empty-todo").style.display = 'none'; 
-      //   }
-      // }
-      // emptyList(); 
         
+
       //items left detailsBox
       function itemsLeftText(){
         itemsLeft.textContent = newtodo.todo.filter(t => t.completed == false).length;
@@ -97,80 +86,80 @@ class Todos{
         newtodo.render(ul);
       }  
       deleteit.addEventListener('click',deleteTodo)
-
       // //completed delete todo
+
 
       //toggle todo
       const toggle = function({target}){
         newtodo.todo = newtodo.todo.map(t =>{
-           if(target.dataset.key == t.id){
-             t.completed = !t.completed;
-           }
-           return t;
-          });
+          if(target.dataset.key == t.id){
+            t.completed = !t.completed;
+          }
+          return t;
+        });
           newtodo.render(ul);
       }
       input.addEventListener('click',toggle);
       //completed toggle todo
     })
-    }
   }
+}
 
 
-
-  let newtodo = new Todos;
+let newtodo = new Todos;
 
 
 //to add a todo by enter
+const enterTodo = function(event){
 
-  const enterTodo = function(event){
 
 // set max 20 todos
 //ingnore empty enter
-   if(event.keyCode == 13 && newtodo.todo.length < 20 && mainInput.value !==""){
-//check if the input todo already exists, it will not be added to the list, but show a message.
-      if(newtodo.todo.some(todo => todo.name == mainInput.value.toLowerCase())) {
+  if(event.keyCode == 13 && newtodo.todo.length < 20 && mainInput.value !==""){
+    
+
+  //check if the input todo already exists, it will not be added to the list, but show a message.
+    if(newtodo.todo.some(todo => todo.name == mainInput.value.toLowerCase())) {
       alert('This task already exists.');
       return;
-      };
+    };
   
-      // newtodo.name = event.target.value 
-      newtodo.addTodo();
-      event.target.value = '';
-      newtodo.render(ul);
-    }
-   
-  }
 
-  mainInput.addEventListener('keyup', enterTodo)
-  
+    // newtodo.name = event.target.value 
+    newtodo.addTodo();
+    event.target.value = '';
+    newtodo.render(ul);
+  }
+}
+mainInput.addEventListener('keyup', enterTodo)  
 //completed adding todo by enter
 
 
+newtodo.render(ul);
 
-  newtodo.render(ul);
 
-  function myFunction() {
-// Declare variables
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById('searchInput');
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = document.getElementsByTagName('li');
-// Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < li.length; i++) {
-// fixed search issue by deleting .getElementsByTagName("li")[0] which was after li[i] on line 69
-      a = li[i];
-      txtValue = a.textContent || a.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        li[i].style.display = "";
-      } else {
-        li[i].style.display = "none";
-      }
+//Add search function
+function myFunction() {
+  // Declare variables
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('searchInput');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  li = document.getElementsByTagName('li');
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    // fixed search issue by deleting .getElementsByTagName("li")[0] which was after li[i] on line 69
+    a = li[i];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
     }
   }
-  
-  myFunction();
+}
+myFunction();
+//Completed search function
 
   
 
